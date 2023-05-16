36e3ed33722f8bbce32574ea4a94859299b89e0a
Рыбаков Андрей <AndreyRV91@gmail.com>
2023-05-16 20:50:49 +0600

    tests for BaseInput

--- src/components/BaseInput/BaseInput.test.ts
+++ src/components/BaseInput/BaseInput.test.ts
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/vue';
+import { fireEvent, render, screen } from '@testing-library/vue';
 import '@testing-library/jest-dom';
 import BaseInput from './BaseInput.vue';
 
@@ -21,5 +21,83 @@ describe('BaseInput', () => {
     expect(input).toHaveAttribute('name', 'inputName');
     expect(input).toHaveAttribute('id', 'inputName');
     expect(input).toHaveAttribute('placeholder', 'Input Placeholder');
+  });
+
+  test('updates input value and emits update event on input', async () => {
+    const wrapper = render(BaseInput, {
+      props: {
+        modelValue: '',
+        name: 'inputName',
+        label: 'Input Label',
+      },
+    });
+
+    const input = screen.getByRole('textbox');
+    expect(input).toHaveValue('');
+
+    await fireEvent.update(input, 'New Value');
+
+    expect(input).toHaveValue('New Value');
+
+    expect(wrapper.emitted()).toHaveProperty('update:modelValue');
+    expect(wrapper.emitted()['update:modelValue']).toHaveLength(1);
+    expect(wrapper.emitted()['update:modelValue'][0]).toEqual(['New Value']);
+  });
+
+  test('formats input value for decimal inputs', async () => {
+    render(BaseInput, {
+      props: {
+        modelValue: '',
+        name: 'inputName',
+        label: 'Input Label',
+        isDecimal: true,
+      },
+    });
+
+    const input = screen.getByRole('textbox');
+    await fireEvent.update(input, '1a2.3b4');
+    expect(input).toHaveValue('12.34');
+
+    await fireEvent.update(input, '1.234');
+    expect(input).toHaveValue('1.234');
+  });
+
+  test('renders error messages', () => {
+    render(BaseInput, {
+      props: {
+        modelValue: '',
+        name: 'inputName',
+        label: 'Input Label',
+        errors: [
+          {
+            $uid: 'error1',
+            $message: 'Error 1',
+          },
+          {
+            $uid: 'error2',
+            $message: 'Error 2',
+          },
+        ],
+      },
+    });
+
+    const error1 = screen.getByText('Error 1');
+    const error2 = screen.getByText('Error 2');
+    expect(error1).toBeInTheDocument();
+    expect(error2).toBeInTheDocument();
+  });
+
+  test('renders hint message', () => {
+    render(BaseInput, {
+      props: {
+        modelValue: '',
+        name: 'inputName',
+        label: 'Input Label',
+        hint: 'Hint Message',
+      },
+    });
+
+    const hint = screen.getByText('Hint Message');
+    expect(hint).toBeInTheDocument();
   });
 });
